18a9c3d6e686a5fcbbf9a96e4b211fb5355fcc07
Felipe <fegomezl@unal.edu.co>
2021-04-03 18:04:00 -0500

    Finally

--- step-1/code/assemble.cpp
+++ step-1/code/assemble.cpp
@@ -3,35 +3,34 @@
 double rhs(const Vector &x){
     double r_2 = pow(x(0),2)+pow(x(1),2);
     double z_2 = pow(x(2),2);
-    return 2*(z_2 - pow(height,2)) + r_2 - pow(out_rad,2);
+    return 2*(pow(height,2) - z_2) + pow(out_rad,2) - z_2;
 }
 
 double g(const Vector &x){
     double z_2 = pow(x(2),2);
-    return int_rad*(pow(height,2) - z_2);
+    return int_rad*(z_2 - pow(height,2));
 }
 
 double exact(const Vector &x){
     double r_2 = pow(x(0),2)+pow(x(1),2);
     double z_2 = pow(x(2),2);
-    return 0.5*(pow(height,2) - z_2)*(r_2 - pow(out_rad,2));
+    return 0.5*(z_2 - pow(height,2))*(r_2 - pow(out_rad,2));
 }
 
 void Artic_sea::assemble_system(){
     //Set the boundary values
     Array<int> ess_tdof_list;
-    Array<int> nbc_marker;
-    if (pmesh->bdr_attributes.Size()){
-        //dirchlet(essential) boundary conditions
-        Array<int> ess_bdr(pmesh->bdr_attributes.Max());
-        ess_bdr = 1;
-        fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
 
-        //Neumann boundary conditions
-        nbc_marker.SetSize(pmesh->bdr_attributes.Max());
-        nbc_marker = 0;
-        nbc_marker[12] = 1;
-    }
+    //Dirchlet(essential) boundary conditions
+    Array<int> ess_bdr(pmesh->bdr_attributes.Max());
+    ess_bdr = 0;
+    ess_bdr[9] = ess_bdr[11] = 1;
+    fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+
+    //Neumann boundary conditions
+    Array<int> nbc_marker(pmesh->bdr_attributes.Max());
+    nbc_marker = 0;
+    nbc_marker[12] = 1;
 
     //Define biliniar form
     a = new ParBilinearForm(fespace);
