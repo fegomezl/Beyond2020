4502cc940830c20cc76885a9b6a3b9fe939719be
Carlos Andrés del Valle <cdelv@hercules4.biocomputo.unal.edu.co>
2021-04-29 09:59:16 -0500

    primer run en el cluster

--- local_config.mk
+++ local_config.mk
@@ -1,8 +1,7 @@
 #Computer parameters
-MFEM_INSTALL_DI/homes/orionsan/cdelv/MFEM_install/mfem-4.2/config/config.mk
-
+MFEM_INSTALL_DIR = /homes/orionsan/cdelv/MFEM_install/mfem-4.2/config/config.mk
 SHARE_DIR = /homes/orionsan/cdelv/brinicle/Share
-PROCCESORS = 16
+PROCCESORS = 8
 
 #Add variables from MFEM
 CONFIG_MK = $(MFEM_INSTALL_DIR) #/share/mfem/config.mk
