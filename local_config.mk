908e6bc37140d977dd407e9f13bc904cb48dbcf5
Carlos Andrés del Valle <cdelv@hercules1.biocomputo.unal.edu.co>
2021-04-29 09:27:55 -0500

    .

--- local_config.mk
+++ local_config.mk
@@ -1,9 +1,9 @@
 #Computer parameters
-MFEM_INSTALL_DIR =~/Library/spack/opt/spack/linux-pop20-excavator/gcc-10.2.0/mfem-4.2.0-ie7sv2es2hutigzmv6ppypyrot2vx7k5
+MFEM_INSTALL_DI/homes/orionsan/cdelv/MFEM_install/mfem-4.2/config/config.mk
 
-SHARE_DIR = ~/Pictures/Beyond
-PROCCESORS = 4
+SHARE_DIR = /homes/orionsan/cdelv/brinicle/Share
+PROCCESORS = 16
 
 #Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)#/share/mfem/config.mk
+CONFIG_MK = $(MFEM_INSTALL_DIR) #/share/mfem/config.mk
 include $(CONFIG_MK)
