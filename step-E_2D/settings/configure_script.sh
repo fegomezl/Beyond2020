275c8e0f5620ebc50338b0abc851a94978b34cc5
Felipe <fegomezl@unal.edu.co>
2021-05-01 22:46:21 -0500

    Added - to makefile

--- step-E_2D/settings/configure_script.sh
+++ step-E_2D/settings/configure_script.sh
@@ -7,9 +7,9 @@ Rmin=$(sed -n 3p settings/parameters.txt | tr -d -c 0-9.)
 sed -i "/Rmin =/c Rmin = $Rmin;" $Mesh_script
 Rmax=$(sed -n 4p settings/parameters.txt | tr -d -c 0-9.)
 sed -i "/Rmax =/c Rmax= $Rmax;" $Mesh_script
-Zmin=$(sed -n 5p settings/parameters.txt | tr -d -c 0-9.)
+Zmin=$(sed -n 5p settings/parameters.txt | tr -d -c 0-9.-)
 sed -i "/Zmin =/c Zmin = $Zmin;" $Mesh_script
-Zmax=$(sed -n 6p settings/parameters.txt | tr -d -c 0-9.)
+Zmax=$(sed -n 6p settings/parameters.txt | tr -d -c 0-9.-)
 sed -i "/Zmax =/c Zmax = $Zmax;" $Mesh_script
 
 NR=$(sed -n 7p settings/parameters.txt | tr -d -c 0-9.)
