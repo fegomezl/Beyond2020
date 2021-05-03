96b7cd382f564db1c82d6671d8959eb97ba84272
Carlos <cdelv@unal.edu.co>
2021-05-03 17:30:19 -0500

    corrección y adición de parametros en el reporte

--- step-E_2D/code/exact.cpp
+++ step-E_2D/code/exact.cpp
@@ -5,13 +5,11 @@ double initial_condition(double r, double z, int m, int n, double a, double b)
   return r*sin(n*M_PI*z/a)*boost::math::cyl_bessel_j(0,boost::math::cyl_bessel_j_zero(0.0,m)*r/b);
 }
 
-void Coe(double a, double b, std::vector<double> & Coeficients)
+void Calc_Coe(double a, double b, std::vector<double> & Coeficients)
 {
-  cout<<"Calculating coefficients of exact solution....";
   for(int m=1; m<=Mterms; m++)
     for(int n=1; n<=Nterms; n++) 
         Coeficients[(m-1)+Mterms*(n-1)]=4*integrate(m,n,a,b)/(a*pow(b*boost::math::cyl_bessel_j(1.0, boost::math::cyl_bessel_j_zero(0.0,m)),2));
-    cout <<"Done!"<<endl;
 }
 
 
@@ -27,8 +25,8 @@ double integrate(int m, int n,double a,double b)
     auto inner_integral = [m,n,a,b](double z)
         {
           auto f = [z,m,n,a,b](double r) { return integrand(r,z,m,n,a,b);} ;
-          return boost::math::quadrature::trapezoidal(f,0.0,a,1e-6);};
-    return boost::math::quadrature::trapezoidal(inner_integral,0.0,b,1e-6);
+          return boost::math::quadrature::trapezoidal(f,0.0,a,1e-8);};
+    return boost::math::quadrature::trapezoidal(inner_integral,0.0,b,1e-8);
 }
 
 double Aux( const Vector &x, double t)
@@ -77,6 +75,7 @@ void print_initial()
         	myfile << r << "\t" << z << "\t" << initial_condition( r, z, 1, 1, Zmax, Rmax) << endl;}}
      myfile.close();
 }
+
 double Aux2( double r, double z, double t)
 {
   int b=Rmax;
@@ -94,10 +93,4 @@ double Aux2( double r, double z, double t)
 
   return sum;
 }
-void print_coefficients()
-{
-	ofstream myfile;
-	myfile.open ("results/coeficients.txt");
-	for(int i=0; i<Mterms*Nterms; i++)
-		myfile << Coeficients[i] <<endl;
-}
+
