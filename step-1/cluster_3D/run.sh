17d0f78c4d7e58a8f362196e8f6d76e8bfedba3c
Julian David Jimenez Paz <jujimenezp@hercules1.biocomputo.unal.edu.co>
2021-05-13 18:34:38 -0500

    run.sh

--- step-1/cluster_3D/run.sh
+++ step-1/cluster_3D/run.sh
@@ -3,8 +3,8 @@
 ## la bandera -cwd quiere decir, trabajar en el directorio donde esta el script
 ##la ejecucion desde la linea de comandos es -> qsub -v order=1,refinements=1,solver=12 run.sh
 #!/bin/bash
-#$ -pe mpi 8
-#$ -q normal.q
+#$ -pe mpi 10
+#$ -q normal.q@hercules1
 #$ -cwd
 #$ -S /bin/bash
 #$ -o ./file.out
@@ -12,4 +12,5 @@
 ##EJ.####
 ##mpiexec -v -np $NSLOTS  --oversubscribe -host hercules4,hercules3,hercules2 ./pi
 
-mpiexec -v -np $NSLOTS -host hercules4,hercules3,hercules2 ~/brinicle/step-1/cluster_3D/./main.x --mesh ~/brinicle/step-1/cluster_3D/results/mesh.msh --internal-radius 5 --outer-radius 10 -he 10 -o 1 -r 2
+mpiexec -v -np $NSLOTS --oversubscribe -host hercules1\
+	~/brinicle/step-1/cluster_3D/./main.x --mesh ~/brinicle/step-1/cluster_3D/results/mesh.msh --internal-radius 5 --outer-radius 10 -he 10 -o 1 -r 5
