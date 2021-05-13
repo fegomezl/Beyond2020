e35c788d2172c404a93c08b141643fa926e080e6
Felipe <fegomezl@unal.edu.co>
2021-05-13 15:31:34 -0500

    Nothing new

--- local_config_cluster.mk
+++
@@ -1,8 +0,0 @@
-#Computer parameters
-MFEM_INSTALL_DIR = ~/MFEM_install/mfem-4.2/
-SHARE_DIR = 
-PROCCESORS = 8
-
-#Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)/config/config.mk
-include $(CONFIG_MK)
