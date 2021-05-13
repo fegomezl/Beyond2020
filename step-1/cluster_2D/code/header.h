c96e9261e7794cd547f3987718111d4ab92dd150
jujimenezp <jujimenezp@unal.edu.co>
2021-05-13 11:26:56 -0500

    Cluster versions of step 1 from branch felipe

---
+++ step-1/cluster_2D/code/header.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "mfem.hpp"
+
+struct Config{
+    //Constructor
+    Config(bool master, int nproc);
+    //Passing parameters
+    bool master;
+    int nproc;
+    int order;
+    int refinements;
+};
+
+using namespace std;
+using namespace mfem;
+
+class Artic_sea{
+    public:
+        Artic_sea(Config config);
+        void run(const char *mesh_file, double &H_min, double &Total_time, double &Mean_error);
+        ~Artic_sea();
+    private:
+        void make_grid(const char *mesh_file);
+        void assemble_system();
+        void solve_system();
+
+        //Global parameters
+        Config config;
+
+        //Output parameters
+        int dim;
+        int serial_refinements;
+        HYPRE_Int size;
+        double h_min;
+        double l2_error;
+
+        //Mesh objects
+        ParMesh *pmesh;
+        FiniteElementCollection *fec;
+        ParFiniteElementSpace *fespace;
+
+        //System objects
+        ParBilinearForm *a;
+        ParLinearForm *b;
+        ParGridFunction *x;
+        FunctionCoefficient u;
+        FunctionCoefficient r;
+
+        //Solver objects
+        HypreParMatrix A;
+        Vector B;
+        Vector X;
+};
+
+extern double rf(const Vector &x);
+
+extern double exact(const Vector &x);
+
+extern double height;
+extern double int_rad;
+extern double out_rad;
