6298259959b7d0c882d4a976980915683833f7c4
Tapemess <felipegomez0224@gmail.com>
2021-05-25 20:07:37 -0500

    changed direction of the mfem library

--- local_config.mk
+++ local_config.mk
@@ -1,5 +1,6 @@
 #Computer parameters
-MFEM_INSTALL_DIR = ~/repos/spack/opt/spack/linux-arch-sandybridge/gcc-10.2.0/mfem-4.2.0-4bqzidc6xxuyfw2pfocmtt5e7eumymps
+MFEM_INSTALL_DIR = ~/repos/spack/opt/spack/linux-arch-sandybridge/gcc-11.1.0/mfem-4.2.0-45ug5plenaqmqep5lwawm2hkivet3eh7
+
 SHARE_DIR = /media/sf_Arch_data/
 PROCCESORS = 4
 
