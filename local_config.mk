8f23b413a02f062b1559bb8b97f2a4a7e9a0fd0d
cdelv <cdelv@unal.edu.co>
2021-06-26 15:46:46 -0500

    .

--- local_config.mk
+++ local_config.mk
@@ -1,8 +1,11 @@
 #Computer parameters
-MFEM_INSTALL_DIR = ~/repos/spack/opt/spack/linux-arch-sandybridge/gcc-10.2.0/mfem-4.2.0-4bqzidc6xxuyfw2pfocmtt5e7eumymps
-SHARE_DIR = /media/sf_Arch_data/
+#MFEM_INSTALL_DIR = ~/Library/spack/opt/spack/linux-pop20-excavator/gcc-10.2.0/mfem-4.2.0-linjgmwmtphlcfh4ow6hopjpq4aw47oz
+
+MFEM_INSTALL_DIR =/home/wind/Library/MFEM/mfem/build/config
+SHARE_DIR = 
 PROCCESORS = 4
 
 #Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
+#CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
+CONFIG_MK = $(MFEM_INSTALL_DIR)/config.mk
 include $(CONFIG_MK)
