7f03dfdc78080f6dc69ac677d7f275e6180ccee6
Carlos <wind@pop-os.localdomain>
2021-07-06 21:08:14 -0500

    .

--- local_config.mk
+++ local_config.mk
@@ -1,5 +1,5 @@
 #Computer parameters
-MFEM_INSTALL_DIR = /opt/spack/opt/spack/linux-pop21-icelake/gcc-10.3.0/mfem-4.2.0-t6yk4fljvrvgg5gh6x45sr7i3ceq7trl
+MFEM_INSTALL_DIR = /opt/spack/opt/spack/linux-pop21-icelake/gcc-10.3.0/mfem-4.2.0-j2kudzxp2l65t32h5zwqum7tjff2qqqn
 
 SHARE_DIR = 
 PROCCESORS = 4
