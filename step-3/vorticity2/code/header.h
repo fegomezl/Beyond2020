c005ab9df3dd2ac8067070981786c9e80da44e95
Felipe <fegomezl@unal.edu.co>
2021-07-11 01:56:11 -0500

    small try

---
+++ step-3/vorticity2/code/header.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "mfem.hpp"
+
+struct Config{
+    //Constructor
+    Config(bool master, int nproc);
+
+    //Passing parameters
+    bool master;
+    int nproc;
+
+    int order;
+    int refinements;
+    bool last;
+
+    double T_f;
+    double invDeltaT;
+    double viscosity;
+    double cold_porosity;
+};
+
+using namespace std;
+using namespace mfem;
+
+class Artic_sea{
+    public:
+        Artic_sea(Config config);
+        void run(const char *mesh_file);
+        ~Artic_sea();
+    private:
+        void make_grid(const char *mesh_file);
+        void assemble_system();
+        void solve_system();
+        void output_results();
+
+        //Global parameters
+        Config config;
+
+        //Output parameters
+        int dim;
+        int serial_refinements;
+        HYPRE_BigInt size;
+        HYPRE_BigInt size_v;
+        double h_min;
+
+        //Mesh objects
+        ParMesh *pmesh;
+
+        FiniteElementCollection *fec;
+        FiniteElementCollection *fec_v;
+
+        ParFiniteElementSpace *fespace;
+        ParFiniteElementSpace *fespace_v;
+
+        Array<int> block_true_offsets;
+
+        //System objects
+        ParGridFunction *w;
+        ParGridFunction *psi;
+        ParGridFunction *v;
+
+        ParGridFunction *theta;
+
+        ParLinearForm *g;
+        ParLinearForm *f;
+
+        ParBilinearForm *m;
+        ParBilinearForm *d;
+        ParMixedBilinearForm *c;
+        ParMixedBilinearForm *ct;
+
+        //Solver objects
+        BlockVector X;
+        BlockVector B;
+
+        HypreParMatrix *M;
+        HypreParMatrix *D;
+        HypreParMatrix *C;
+        HypreParMatrix *Ct;
+};
+
+extern double height;
+extern double int_rad;
+extern double out_rad;
+
+extern double border;
+extern double InvR;
