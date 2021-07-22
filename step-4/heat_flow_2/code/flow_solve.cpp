afa9d0ed7bb332252fd8ef70d79ba2d5d8eaf24a
Felipe <fegomezl@unal.edu.co>
2021-07-22 12:23:09 -0500

    added 3

--- step-4/heat_flow_2/code/flow_solve.cpp
+++ step-4/heat_flow_2/code/flow_solve.cpp
@@ -34,6 +34,7 @@ void Flow_Operator::Solve(const HypreParVector *Theta){
     //Solve the linear system Ax=B
     Y.Randomize();
     superlu.Mult(B, Y);
+    superlu.DismantleGrid();
 
     //Recover the solution on each proccesor
     w->Distribute(&(Y.GetBlock(0)));
