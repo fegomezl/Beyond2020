da1b6903770f8fdf4583f5aedcc3601c551131ad
Felipe <fegomezl@unal.edu.co>
2021-08-27 17:44:53 -0500

    changed directory

--- local_config.mk
+++ local_config.mk
@@ -1,5 +1,5 @@
 #Computer parameters
-MFEM_INSTALL_DIR = ~/Repos/spack/opt/spack/linux-arch-sandybridge/gcc-11.1.0/mfem-4.2.0-di22qif3vvteq46ntd7jrouwbctljvu6/
+MFEM_INSTALL_DIR = ~/Repos/spack/opt/spack/linux-arch-sandybridge/gcc-11.1.0/mfem-develop-ih7axzrrz5if4aau5gfee62i2u6o5ham
 
 SHARE_DIR = /media/sf_Archdata/
 PROCCESORS = 4
