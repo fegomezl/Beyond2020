666c44634a53c502452e108f4b0c89df40076150
Felipe <fegomezl@unal.edu.co>
2021-09-15 15:28:02 -0500

    erased old files

--- local_config.mk
+++
@@ -1,8 +0,0 @@
-#Computer parameters
-MFEM_INSTALL_DIR = ~/spackgcc/mfem-4.2.0-memory-superlu-ck7pgi4afqck7k6dn2w4cisxfk7db3l6/
-SHARE_DIR = /media/sf_Shared_Folder_Arch2/
-PROCCESORS = 6
-
-#Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
-include $(CONFIG_MK)
