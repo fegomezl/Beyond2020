8f681f0475c6ba0a0455b9e1aee47ededcfc440b
Felipe <fegomezl@unal.edu.co>
2021-09-15 15:23:57 -0500

    erased old files

--- local_config.mk
+++
@@ -1,9 +0,0 @@
-#Computer parameters
-MFEM_INSTALL_DIR = /opt/spack/opt/spack/linux-pop21-icelake/gcc-10.3.0/mfem-develop-rggtpllz3jjvunjhyjvhcyvdgicu7b7a
-
-SHARE_DIR = 
-PROCCESORS = 4
-
-#Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
-include $(CONFIG_MK)
