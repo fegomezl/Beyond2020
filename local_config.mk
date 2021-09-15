8ebd4cf2276e14b759f0f6f943e0d40f8f27fbd0
Felipe <fegomezl@unal.edu.co>
2021-09-15 15:02:20 -0500

    Removed previous verions

--- local_config.mk
+++
@@ -1,9 +0,0 @@
-#Computer parameters
-MFEM_INSTALL_DIR = ~/Repos/spack/opt/spack/linux-arch-sandybridge/gcc-11.1.0/mfem-develop-ih7axzrrz5if4aau5gfee62i2u6o5ham
-
-SHARE_DIR = /media/sf_Archdata/
-PROCCESORS = 4
-
-#Add variables from MFEM
-CONFIG_MK = $(MFEM_INSTALL_DIR)/share/mfem/config.mk
-include $(CONFIG_MK)
