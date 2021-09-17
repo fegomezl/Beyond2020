38d097fde817d9418bafd015f24447007a7f9100
Felipe <fegomezl@unal.edu.co>
2021-09-17 08:35:28 -0500

    added becnhmark test for time dependendt

---
+++ Time_Dependent/twophase_u/code/assemble.cpp
@@ -0,0 +1,144 @@
+#include "header.h"
+
+void Artic_sea::assemble_system(){
+    //Initialize the system
+    t = 0;
+    dt = config.dt_init;
+    last = false;
+    vis_steps = config.vis_steps_max;
+    vis_impressions = 0;
+
+    //Set boundary conditions
+    ess_bdr.SetSize(pmesh->bdr_attributes.Max());
+    ess_bdr = 0;  ess_bdr[0] = 1;  ess_bdr[1] = 1;
+
+    Array<int> ess_bdr_aux(pmesh->bdr_attributes.Max());
+    ess_bdr_aux = 0; ess_bdr_aux[0] = 1;
+
+    //Define solution x and apply initial conditions
+    x = new ParGridFunction(fespace);
+    ConstantCoefficient initial_aux(T_0);
+    x->ProjectCoefficient(initial_f);
+    x->ProjectBdrCoefficient(initial_f, ess_bdr);
+    x->ProjectBdrCoefficient(initial_aux, ess_bdr_aux);
+    X = new HypreParVector(fespace);
+    x->GetTrueDofs(*X);
+
+    oper = new Conduction_Operator(config, *fespace, *X, ess_bdr);
+
+    //Set the ODE solver type
+    switch (config.ode_solver_type){
+        // MFEM explicit methods
+        case 1: ode_solver = new ForwardEulerSolver; break;
+        case 2: ode_solver = new RK2Solver(0.5); break;
+        case 3: ode_solver = new RK3SSPSolver; break;
+        case 4: ode_solver = new RK4Solver; break;
+        // MFEM implicit L-stable methods
+        case 5: ode_solver = new BackwardEulerSolver; break;
+        case 6: ode_solver = new SDIRK23Solver(2); break;
+        case 7: ode_solver = new SDIRK33Solver; break;
+        // CVODE
+        case 8: cvode = new CVODESolver(MPI_COMM_WORLD, CV_ADAMS); break;
+        case 9: cvode = new CVODESolver(MPI_COMM_WORLD, CV_BDF); break;
+        // ARKODE
+        case 10: arkode = new ARKStepSolver(MPI_COMM_WORLD, ARKStepSolver::EXPLICIT); break;
+        case 11: arkode = new ARKStepSolver(MPI_COMM_WORLD, ARKStepSolver::EXPLICIT); break;
+        case 12: arkode = new ARKStepSolver(MPI_COMM_WORLD, ARKStepSolver::IMPLICIT); break;
+        default:
+                 cout << "Unknown ODE solver type: " << config.ode_solver_type << "\n"
+                      << "Setting ODE to BackwardEulerSolver.\n";
+                 config.ode_solver_type = 1;
+                 ode_solver = new BackwardEulerSolver; break;
+    }
+
+    // Initialize ODE solver
+    if (config.ode_solver_type < 8)
+        ode_solver->Init(*oper);
+    else if (cvode){
+        cvode->Init(*oper);
+        cvode->SetSStolerances(config.reltol, config.abstol);
+        cvode->SetMaxStep(dt);
+        cvode->SetStepMode(CV_ONE_STEP);
+        ode_solver = cvode;
+    }
+    else if (arkode){
+        arkode->Init(*oper);
+        arkode->SetSStolerances(config.reltol, config.abstol);
+        arkode->SetMaxStep(dt);
+        arkode->SetStepMode(ARK_ONE_STEP);
+        if (config.ode_solver_type == 11) arkode->SetERKTableNum(FEHLBERG_13_7_8);
+        ode_solver = arkode;
+    }
+
+    //Open the paraview output and print initial state
+    string folder = "results/" + to_string(config.refinements) + "_" + to_string(config.nDeltaT); 
+    paraview_out = new ParaViewDataCollection(folder, pmesh);
+    paraview_out->SetDataFormat(VTKFormat::BINARY);
+    paraview_out->SetLevelsOfDetail(config.order);
+    paraview_out->RegisterField("Temperature", x);
+    paraview_out->SetCycle(vis_impressions);
+    paraview_out->SetTime(t);
+    paraview_out->Save();
+
+    //Start program check
+    if (config.master)
+        cout << left << setw(12)
+             << "--------------------------------------------------\n"
+             << left << setw(12)
+             << "Step" << setw(12)
+             << "Dt" << setw(12)
+             << "Time" << setw(12)
+             << "Progress"
+             << left << setw(12)
+             << "\n--------------------------------------------------\n";
+
+}
+
+double initial(const Vector &x){
+    return T_i;
+}
+
+double rf(const Vector &x){
+    return x(0);
+}
+
+Conduction_Operator::Conduction_Operator(Config config, ParFiniteElementSpace &fespace, const Vector &X, Array<int> ess_bdr):
+    TimeDependentOperator(fespace.GetTrueVSize(), 0.),
+    config(config),
+    fespace(fespace),
+    m(NULL),
+    k(NULL),
+    t(NULL),
+    aux(&fespace),
+    aux_C(&fespace),
+    aux_K(&fespace),
+    r(rf),
+    coeff_C(&aux_C), coeff_rC(r, coeff_C),
+    coeff_K(&aux_K), coeff_rK(r, coeff_K), dt_coeff_rK(1., coeff_rK),
+    coeff_L(&aux), coeff_rL(r, coeff_L),
+    M_solver(fespace.GetComm()),
+    T_solver(fespace.GetComm())
+{
+    const double rel_tol = 1e-8;
+
+    fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+
+    //Configure M solver
+    M_solver.iterative_mode = false;
+    M_solver.SetRelTol(rel_tol);
+    M_solver.SetAbsTol(0.);
+    M_solver.SetMaxIter(100);
+    M_solver.SetPrintLevel(0);
+    M_solver.SetPreconditioner(M_prec);
+    M_prec.SetType(HypreSmoother::Jacobi);
+
+    //Configure T solver
+    T_solver.iterative_mode = false;
+    T_solver.SetRelTol(rel_tol);
+    T_solver.SetAbsTol(0.);
+    T_solver.SetMaxIter(100);
+    T_solver.SetPrintLevel(0);
+    T_solver.SetPreconditioner(T_prec);
+
+    SetParameters(X);
+}
