81be94183ffdac56b5fb7abce5ab86e6d4b90124
Felipe <fegomezl@unal.edu.co>
2021-09-28 14:32:16 -0500

    Added script for analysis

---
+++ Time_Dependent/twophase_exact/settings/run.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+# Refresh parameters
+
+File='settings/parameters.txt'
+
+rm -rf Data
+mkdir -p Data
+
+for ii in 1 2 3 4 5 6 7 8 9
+do 
+    for jj in 1 2 3 4 5 6 7 8 9
+    do 
+        Rmin=$(sed -n 3p settings/parameters.txt | tr -d -c 0-9.-)
+        sed -i "26s/.*/$ii          #nDeltaT/" $File
+        sed -i "27s/.*/$jj          #nEpsilonT/" $File
+        make
+        make plot
+        cp results/data.txt Data/d_${ii}e_${jj}.txt
+    done
+done
