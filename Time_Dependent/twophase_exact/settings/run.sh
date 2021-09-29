d75e8c15d4d309d2ea5e1a92b31b5c0eba9d8d7d
cdelv <cdelv@unal.edu.co>
2021-09-28 19:46:19 -0500

    datos del 1 script

--- Time_Dependent/twophase_exact/settings/run.sh
+++ Time_Dependent/twophase_exact/settings/run.sh
@@ -6,9 +6,9 @@ File='settings/parameters.txt'
 rm -rf Data
 mkdir -p Data
 
-for ii in 1 2 3 4 5 6 7 8 9
+for ii in 1 2 3 4 5
 do 
-    for jj in 1 2 3 4 5 6 7 8 9
+    for jj in 1 2 3 4 5
     do 
         Rmin=$(sed -n 3p settings/parameters.txt | tr -d -c 0-9.-)
         sed -i "26s/.*/$ii          #nDeltaT/" $File
@@ -16,6 +16,6 @@ do
         make
         make plot
         cp results/data.txt Data/d_${ii}e_${jj}.txt
-        make clean
+        rm -rf results/graph
     done
 done
