cb4ad95cd3226a3e3565fd6e646ec80e65204741
fegomezl <fegomezl@unal.edu.co>
2022-05-24 22:49:29 -0500

    reordered branch

--- Brinicle/brinicle_3d/settings/cylinder1.geo
+++
@@ -1,6 +0,0 @@
-// Gmsh project created on Sun Mar 13 16:06:19 2022
-SetFactory("OpenCASCADE");
-//+
-R=10;
-Z=20;
-Cylinder(1) = {0, 0, 0, 0, 0, Z, R, 2*Pi};
--- Brinicle/brinicle_original/settings/send.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PROCCESORS_O=16
-
-qsub -cwd -S /bin/bash -q normal.q@hercules2 -N brinicle -o results/brinicle.out -e results/brinicle.err -pe make $PROCCESORS_O settings/job.sh &
