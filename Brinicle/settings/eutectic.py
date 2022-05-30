d567f16efc9411944e7d5ec9b47f8dc6a2b4528d
jujimenezp <jujimenezp@unal.edu.co>
2022-05-30 10:49:07 -0500

    contour line and colorbar label in eutectic.py

--- Brinicle/settings/eutectic.py
+++ Brinicle/settings/eutectic.py
@@ -3,16 +3,16 @@ import numpy as np
 import itertools
 import matplotlib.pyplot as plt
 
-with open('settings/parameters.txt','r') as file:
+with open('settings/parameters.mk','r') as file:
     for l in file.readlines():
         if 'Initial_temperature' in l:
-            Initial_temperature = float(l.split(' ',1)[0])
+            Initial_temperature = float(l.split(' ')[2])
         if 'Inflow_temperature' in l:
-            Inflow_temperature = float(l.split(' ',1)[0])
+            Inflow_temperature = float(l.split(' ')[2])
         if 'Initial_salinity' in l:
-            Initial_salinity = float(l.split(' ',1)[0])
+            Initial_salinity = float(l.split(' ')[2])
         if 'Inflow_salinity' in l:
-            Inflow_salinity = float(l.split(' ',1)[0])
+            Inflow_salinity = float(l.split(' ')[2])
 
 with open('code/header.h', 'r') as header:
     for l in header.readlines():
@@ -33,7 +33,7 @@ ZeroSalinity = S0_ref/S_ref
 FusionPoint_a *= S_ref/T_ref
 FusionPoint_b *= np.power(S_ref,3)/T_ref
 
-T, S = np.meshgrid(np.linspace(0, 1, 100), np.linspace(0, 1, 100))
+T, S = np.meshgrid(np.linspace(0, 1, 1000), np.linspace(0, 1, 1000))
 
 T_ = T + ZeroTemperature
 S_ = S + ZeroSalinity
@@ -43,9 +43,13 @@ FusionPoint= np.where(fp_function<0, 0, fp_function)
 
 max = np.amax(FusionPoint)
 fig, ax = plt.subplots(layout='constrained')
-pc = ax.pcolormesh(T, S, FusionPoint, vmin=-max, vmax=max, cmap='RdBu_r')
-fig.colorbar(pc, ax=ax)
+pc = ax.pcolormesh(T, S, FusionPoint, vmin=0, vmax=max, cmap='Reds')
+cbar = plt.colorbar(pc)
+cbar.ax.set_ylabel('Relative Density', rotation = 270)
+
+CS = ax.contour(T, S, FusionPoint, 0, colors='k')
+
 ax.set_title('Eutectic Curve')
 ax.set_xlabel('Temperature')
 ax.set_ylabel('Salinity')
-plt.savefig('results/eutectic_curve.png')
+plt.savefig('results/graph/eutectic.png')
