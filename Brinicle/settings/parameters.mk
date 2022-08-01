f08af43d956346446e297ea6af2e6c0e41393b3d
cdelv <cdelv@unal.edu.co>
2022-07-31 23:41:20 -0500

    mesh

--- Brinicle/settings/parameters.mk
+++ Brinicle/settings/parameters.mk
@@ -9,29 +9,29 @@
 
 #Mesh Parameters:
 SCRIPT = settings/sea.geo    #Mesh_script
-R = 10                  #R(mm)
-Z = 40                  #Z(mm)
+R = 7                  #R(mm)
+Z = 45                  #Z(mm)
 RIn = 2                   #Inflow(mm)
 
 #Simulation parameters:
 DT = 0.001         #Dt(s)
-T_FI = 1           #Final_time(s)
-VIS = 0            #Visualization_steps
+T_FI = 70           #Final_time(s)
+VIS = 400            #Visualization_steps
 
 #FE parameters:
 REF = 4          #Refinements
 ORDER = 1          #Order
 ABST_C= 0          #abstol(Conduction)
-RELT_C = 0.00000001 #reltol(Conduction)
+RELT_C = 0.0000000001 #reltol(Conduction)
 ITER_C = 100        #iter(Conduction)
 ABST_S = 0.00001    #abstol(SUNDIALS)
 RELT_S = 0.00001    #reltol(SUNDIALS)
 EPSILON = 9          #nEpsilon
 
 #Brinicle conditions:
-Q = 300        #FlowRate(mm^3/s)
-Nl = 1          #Nucleation_length(mm)
-Nh = 4          #Nucleation_height(mm)
+Q = 500        #FlowRate(mm^3/s)
+Nl = 0          #Nucleation_length(mm)
+Nh = 0          #Nucleation_height(mm)
 Ti = -2         #Initial_temperature(°C)
 To = -10         #Inflow_temperature(°C)
 Tn = -10         #Nucleation_temperature(°C)
